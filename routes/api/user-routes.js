4128803f18889b856e64b9c228892175ecfdce71
marleyschneiderr <marley33@uw.edu>
2023-05-07 21:55:32 -0700

    User routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+// put in user controller methods
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+// controller methods
+router.get("/users", getAllUser); // GET /api/users
+router.post("/users", createUser); // POST /api/users
+
+router.get("/users/:id", getUserById); // GET /api/users/:id
+router.put("/users/:id", updateUser); // PUT /api/users/:id
+router.delete("/users/:id", deleteUser); // DELETE /api/users/:id
+
+router.post("/users/:userId/friends/:friendId", addFriend); // POST /api/users/:userId/friends/:friendId
+router.delete("/users/:userId/friends/:friendId", removeFriend); // DELETE /api/users/:userId/friends/:friendId
+
+module.exports = router;
