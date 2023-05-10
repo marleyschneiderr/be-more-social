5a7693f483180fa7b322fec2b2f008133e4dcd66
marleyschneiderr <marley33@uw.edu>
2023-05-09 18:01:53 -0700

    Trying to fix routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -12,15 +12,15 @@ const {
 } = require("../../controllers/user-controller");
 
 // controller methods
-router.get("/", getAllUsers); // GET /api/users
-router.post("/", createUser); // POST /api/users
+router.get("/", getAllUsers); // GET/api/users
+router.post("/", createUser); // POST/api/users
 
-router.get("/:id", getUserById); // GET /api/users/:id
-router.put("/:id", updateUser); // PUT /api/users/:id
-router.delete("/:id", deleteUser); // DELETE /api/users/:id
+router.get("/:id", getUserById); // GET/api/users/:id
+router.put("/:id", updateUser); // PUT/api/users/:id
+router.delete("/:id", deleteUser); //DELETE /api/users/:id
 
-router.post("/:userId/friends/:friendId", addFriend); // POST /api/users/:userId/friends/:friendId
-router.delete("/:userId/friends/:friendId", removeFriend); // DELETE /api/users/:userId/friends/:friendId
+router.post("/:userId/friends/:friendId", addFriend); //POST /api/users/:userId/friends/:friendId
+router.delete("/:userId/friends/:friendId", removeFriend); //DELETE /api/users/:userId/friends/:friendId
 
 module.exports = router;
 
